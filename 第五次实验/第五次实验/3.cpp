123c42a813a8e02509f82c623d3b977db46ef31c
3n1ne <L2643311218@outlook.com>
2024-12-16 20:13:05 +0800

    ver1.1

--- 第五次实验/第五次实验/3.cpp
+++ 第五次实验/第五次实验/3.cpp
@@ -1,35 +1,35 @@
-//#include <iostream>
-//using namespace std;
-//class Cuboid
-//{
-//private:
-//	double length;
-//	double width;
-//	double height;
-//public:
-//	void SetV(double a, double b, double c)
-//	{
-//		length = a;
-//		width = b;
-//		height = c;
-//	}
-//	double ShowV()
-//	{
-//		return length * width * height;
-//	}
-//};
-//int main()
-//{
-//	Cuboid C[3];
-//	for (int i = 0; i < 3; i++)
-//	{
-//		double a, b, c;
-//		cin >> a >> b >> c;
-//		C[i].SetV(a, b, c);
-//	}
-//	for (int i = 0; i < 3; i++)
-//	{
-//		cout << "体积是 ：" << C[i].ShowV();
-//	}
-//		
-//}
+#include <iostream>
+using namespace std;
+class Cuboid
+{
+private:
+	double length;
+	double width;
+	double height;
+public:
+	void SetV(double a, double b, double c)
+	{
+		length = a;
+		width = b;
+		height = c;
+	}
+	double ShowV()
+	{
+		return length * width * height;
+	}
+};
+int main()
+{
+	Cuboid C[3];
+	for (int i = 0; i < 3; i++)
+	{
+		double a, b, c;
+		cin >> a >> b >> c;
+		C[i].SetV(a, b, c);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "体积是 ：" << C[i].ShowV();
+	}
+		
+}
